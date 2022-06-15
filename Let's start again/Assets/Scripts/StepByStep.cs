9a4be623d2e91b75dfbec6a8700dbb250cca2f49
KoNNoDIoDa <karevzhenya@gmail.com>
2022-06-15 22:26:12 +0300

    Added a lobby room. Fixed rooms sizes bug
    Fix the amount of hallways!!

--- Let's start again/Assets/Scripts/StepByStep.cs
+++ Let's start again/Assets/Scripts/StepByStep.cs
@@ -8,28 +8,31 @@ public class StepByStep : MonoBehaviour
     List <Node> nodes = new List <Node>();
     public Material mat;
     public Sprite sprite;
-    const int MIN = 6;
+    const int MIN = 7;
+    int iteration = 5;
     Node parent;
+    public bool showRooms = true;
+    RectInt safeRoom = new RectInt();
     // Start is called before the first frame update
     void Start()
     {
         parent = new Node();
         parent.SetParam(new RectInt(0, 0, 90, 90), false);
         //parent.SetParam
-        Split(5, parent, nodes);
+        Split(iteration, parent, nodes);
         for(int i = 0; i < nodes.Count; i++)
         {
             GameObject plate = new GameObject();
             Vector3 position = plate.transform.position;
-            position.x = nodes[i].GetParam(true).x; 
-            position.y = nodes[i].GetParam(true).y;
+            position.x = nodes[i].GetParam(showRooms).x; 
+            position.y = nodes[i].GetParam(showRooms).y;
             position.z = -10;
             
             plate.transform.position = position;
 
             Vector2 scale = plate.transform.localScale;
-            scale.x = nodes[i].GetParam(true).width;
-            scale.y = nodes[i].GetParam(true).height;
+            scale.x = nodes[i].GetParam(showRooms).width;
+            scale.y = nodes[i].GetParam(showRooms).height;
             plate.transform.localScale = scale;
 
             SpriteRenderer rend = plate.AddComponent(typeof(SpriteRenderer)) as SpriteRenderer;
@@ -70,30 +73,51 @@ public class StepByStep : MonoBehaviour
     {
         RectInt sizes = node.GetParam(false);
 
-        if (iterations > 0   && (sizes.width >= 2 * MIN || sizes.height >= 2 * MIN))
+        if (iterations > 0 && (sizes.width >= 2 * MIN || sizes.height >= 2 * MIN))
         {
             
             bool splitH = Random.value > 0.5;
-            if (sizes.width > sizes.height & sizes.width / sizes.height >= 1.25 & sizes.width >= 2 * MIN) splitH = false;
-            else if (sizes.height > sizes.width & sizes.height / sizes.width >= 1.25 & sizes.height >= 2 * MIN) splitH = true;
+            if ((sizes.width > sizes.height && sizes.width / sizes.height >= 1) && sizes.width >= 2 * MIN) splitH = false;
+            else if ((sizes.height > sizes.width && sizes.height / sizes.width >= 1) && sizes.height >= 2 * MIN) splitH = true;
             node.SetParam(true);
             if (splitH)
             {
-                int split = (int) Random.Range(sizes.height * 0.3f, sizes.height * 0.5f);
+                int split = (sizes.height > 2 * MIN ? (int) Random.Range(sizes.height * 0.4f, sizes.height * 0.5f) : MIN);
                 node.lChild.SetParam(new RectInt(sizes.x, sizes.y, sizes.width, split), false);
 
-                node.rChild.SetParam(new RectInt(sizes.x, sizes.y + split, sizes.width, sizes.height - split), false);
+                node.rChild.SetParam(new RectInt(sizes.x, sizes.y + (iterations == iteration ? split + 32 : split), sizes.width, sizes.height - split), false);
+
+                if(iterations == iteration)
+                {
+                    safeRoom.y = split + 2;
+                    safeRoom.height = 28;
+                    safeRoom.width = 15;
+                    safeRoom.x = 100500;
+                }
             }
             else
             {
-                int split = (int) Random.Range(sizes.width*0.3f, sizes.width * 0.5f);
+                int split = (sizes.width > 2 * MIN ? (int) Random.Range(sizes.width*0.4f, sizes.width * 0.6f) : MIN);
                 node.lChild.SetParam(new RectInt(sizes.x, sizes.y, split, sizes.height), false);
 
-                node.rChild.SetParam( new RectInt(sizes.x + split, sizes.y, sizes.width - split, sizes.height), false);
+                node.rChild.SetParam( new RectInt(sizes.x + (iterations == iteration ? split + 32 : split), sizes.y, sizes.width - split, sizes.height), false);
+
+                if (iterations == iteration)
+                {
+                    safeRoom.x = split + 2;
+                    safeRoom.width = 28;
+                    safeRoom.height = 15;
+                    safeRoom.y = 100500;
+                }
             }
+
             Split(iterations - 1, node.lChild, nodes);
             Split(iterations - 1, node.rChild, nodes);
-            CreateCorridors(node, nodes);
+            if (iterations == iteration)
+            {
+                CreateCorridors(node, nodes, true);
+            }
+            else CreateCorridors(node, nodes, false);
         }
         else nodes.Add(node);
         CreateRooms(node);
@@ -101,15 +125,15 @@ public class StepByStep : MonoBehaviour
     void CreateRooms(Node node)
     {
         RectInt sizes = node.GetParam(false);
-        int randX = Random.Range(3, sizes.x / 10);
-        int randY = Random.Range(3, sizes.y / 10);
+        int randX = Random.Range(2, sizes.x / 10);
+        int randY = Random.Range(2, sizes.y / 10);
         int randW = (int) (randX * Random.Range(1f, 2f));
         //randW = randW < sizes.width / 10 ? sizes.width - 2 : randW;
         int randH = (int) (randY * Random.Range(1f, 2f));
         //randH = randH < sizes.height / 10 ? sizes.height - 2 : randH;
-        node.SetParam(new RectInt(sizes.x + randX, sizes.y + randY, (sizes.width - 6 < 3 ? 3 : (sizes.width - 6 > randW ? sizes.width - randW : sizes.width - 6)), (sizes.height - 6 < 3 ? 3 : (sizes.height - 6 > randH ? sizes.height - randH : sizes.height - 6))), true);
+        node.SetParam(new RectInt(sizes.x + randX, sizes.y + randY, (sizes.width - 4 < 3 ? 3 : (sizes.width - 4 > randW ? sizes.width - randW : sizes.width - 4)), (sizes.height - 4 < 3 ? 3 : (sizes.height - 4 > randH ? sizes.height - randH : sizes.height - 4))), true);
     }
-    void CreateCorridors(Node node, List<Node> nodes)
+    void CreateCorridors(Node node, List<Node> nodes, bool sR)
     {
         Node left = FindLeaf(node.lChild);
         Node right = FindLeaf(node.rChild);
@@ -123,6 +147,14 @@ public class StepByStep : MonoBehaviour
             {
                 Node.Hallways hallway = new Node.Hallways();
                 hallway.Add(new RectInt(sizesL.x + sizesL.width/ 2, sizesL.y + sizesL.height / 2, -((sizesL.x + sizesL.width / 2) - (sizesR.x + sizesR.width / 2) + 1), 1), nodes.IndexOf(left));
+                if (sR)
+                {
+                    safeRoom.y = sizesL.y + sizesL.height / 2;
+                    Node theRoom = new Node();
+                    theRoom.SetParam(safeRoom, true);
+                    nodes.Add(theRoom);
+                }
+
                 left.AddHall(hallway);
                 FindAllRooms(node);
             }
@@ -130,6 +162,14 @@ public class StepByStep : MonoBehaviour
             {
                 Node.Hallways hallway = new Node.Hallways();
                 hallway.Add(new RectInt(sizesL.x + sizesL.width / 2, sizesL.y + sizesL.height / 2, 1, -((sizesL.y + sizesL.height / 2) - (sizesR.y  + sizesR.height/ 2) + 1)), nodes.IndexOf(left));
+                if (sR)
+                {
+                    safeRoom.x = sizesL.x + sizesL.width / 2;
+                    Node theRoom = new Node();
+                    theRoom.SetParam(safeRoom, true);
+                    nodes.Add(theRoom);
+                }
+
                 left.AddHall(hallway);
                 FindAllRooms(node);
             }
@@ -142,6 +182,14 @@ public class StepByStep : MonoBehaviour
             hallway.Add(new RectInt(halls[0].x, halls[0].y + halls[0].height, -((sizesL.x + sizesL.width / 2) - (sizesR.x + sizesR.width / 2) + 1), 1));
             left.AddHall(hallway);
             FindAllRooms(node);
+            if (sR)
+            {
+                if (safeRoom.x == 100500) safeRoom.x = sizesL.width / 2 + sizesL.x;
+                else if (safeRoom.y == 100500) safeRoom.y = halls[0].y + halls[0].height;
+                Node theRoom = new Node();
+                theRoom.SetParam(safeRoom, true);
+                nodes.Add(theRoom);
+            }
         }
         else
         {
@@ -151,6 +199,14 @@ public class StepByStep : MonoBehaviour
             hallway.Add(new RectInt(halls[0].x + halls[0].width, halls[0].y, 1, -((sizesL.y + sizesL.height / 2) - (sizesR.y + sizesR.height / 2) + 1)));
             left.AddHall(hallway);
             FindAllRooms(node);
+            if (sR)
+            {
+                if (safeRoom.x == 100500) safeRoom.x = halls[0].x + halls[0].width;
+                else if (safeRoom.y == 100500) safeRoom.y = halls[0].y;
+                Node theRoom = new Node();
+                theRoom.SetParam(safeRoom, true);
+                nodes.Add(theRoom);
+            }
         }
     }
     Node FindLeaf(Node node)
